add147511f7f43fe5435124279ff5f1e4bd3aa5f
Ngalim Siregar <ngalim.siregar@gmail.com>
2019-09-27 13:03:23 +0700

    refactor test format of two sum

--- array/twosum/twosum_test.go
+++ array/twosum/twosum_test.go
@@ -8,16 +8,38 @@ import (
 )
 
 func TestTwoSumArray(t *testing.T) {
-	arr := []int{2, 7, 11, 15}
-	target := 9
-	result := [2]int{2, 7}
+	t.Log("when length >= 2")
+	{
+		t.Logf("\tfind two value which sum is target")
+		{
+			arr := []int{2, 7, 11, 15}
+			target := 9
+			result := [2]int{2, 7}
 
-	assert.Equal(t, result, TwoSum(arr, target))
-}
+			assert.Equal(t, result, TwoSum(arr, target))
+		}
 
-func TestSortArray(t *testing.T) {
-	arr := []int{1}
-	target := 2
+		t.Log("\twhen value not found")
+		{
+			t.Log("\t\treturn array of 0")
+			{
+				arr := []int{4, 7, 11, 15}
+				target := 9
+				result := [2]int{0, 0}
 
-	assert.Panics(t, func() { TwoSum(arr, target) })
+				assert.Equal(t, result, TwoSum(arr, target))
+			}
+		}
+	}
+
+	t.Log("when length < 2")
+	{
+		t.Logf("\traise panic when array length < 2")
+		{
+			arr := []int{1}
+			target := 2
+
+			assert.Panics(t, func() { TwoSum(arr, target) })
+		}
+	}
 }
